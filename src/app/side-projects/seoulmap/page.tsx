93fe46dee4a54b37e8a5c83b9975907412489eb6
YJ <hghk0046@gmail.com>
2026-03-05 12:55:05 +0900

    fix: 마그네틱 버튼 추가 전 폴더 구조 점검

--- src/app/side-projects/seoulmap/page.tsx
+++
@@ -1,8 +0,0 @@
-import seoulData from '@/src/constants/juso/2015/json/seoul_municipalities_geo_simple.json';
-
-export default function SeoulMap() {
-    return (
-        <div>            
-        </div>
-    )
-}
---
+++ types/blackhole.ts
@@ -0,0 +1,6 @@
+export interface Particle {
+    angle: number;
+    distance: number;
+    speed: number;
+    size: number;
+}
