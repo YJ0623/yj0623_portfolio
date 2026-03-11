a5233dda7f60aedf0d726555ab8d671a3fe4518a
YJ <hghk0046@gmail.com>
2026-03-11 15:12:24 +0900

    feat: 맛집 페이지 메타데이터 추가

--- src/app/side-projects/hongik-map/layout.tsx
+++ src/app/side-projects/hongik-map/layout.tsx
@@ -1,4 +1,25 @@
+import { Metadata } from "next";
 import { NavigationBar } from "@/src/components/map/NavigationBar";
+
+export const metadata: Metadata = {
+    title: '홍익 맛집 지도',
+    description: '홍대생들의 진짜 찐 맛집을 공유하고 확인해 보세요!',
+    openGraph: {
+        title: '홍익 맛집 지도',
+        description: '홍대생들의 찐 맛집을 공유하고 확인해 보세요!',
+        siteName: 'Hongik Map',
+        locale: 'ko_KR',
+        type: 'website',
+        images: [
+            {
+                url: '/hongikMapImage',
+                width: 800,
+                height: 600,
+                alt: '홍익 맛집 지도',
+            },
+        ],
+    },
+};
 
 export default function HongikMapLayout({
     children,
